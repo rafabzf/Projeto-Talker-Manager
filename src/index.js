88463ab77b854de3618311e2798a79a8d21048e0
Rafael <rafinhabzf@gmail.com>
2023-01-19 18:58:18 -0300

    feat: req 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const path = './src/talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar.
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const file = await fs.readFile(path, 'utf-8');
+  res.status(HTTP_OK_STATUS).json(JSON.parse(file)); 
 });
 
 app.listen(PORT, () => {
